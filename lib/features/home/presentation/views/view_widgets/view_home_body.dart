793303943ee93cdc4b47757fa983267d1ddc5b9a
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-07-31 20:43:07 +0300

    best seller list view

--- lib/features/home/presentation/views/view_widgets/view_home_body.dart
+++ lib/features/home/presentation/views/view_widgets/view_home_body.dart
@@ -1,6 +1,6 @@
 import 'package:bookbox/core/utils/styles.dart';
+import 'package:bookbox/features/home/presentation/views/view_widgets/best_seller_list_view.dart';
 import 'package:flutter/material.dart';
-import 'best_seller_list_view_item.dart';
 import 'books_list_view.dart';
 import 'custom_app_bar.dart';
 
@@ -9,26 +9,37 @@ class ViewHomeBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          BooksListView(),
-          SizedBox(
-            height: 30,
+    return const  CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomAppBar(),
+              BooksListView(),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  'Best Seller',
+                  style: Styles.textStyle18,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
           ),
-          Text(
-            'Best Seller',
-            style: Styles.textStyle18,
+        ),
+        SliverFillRemaining(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: BestSellerListView(),
           ),
-          SizedBox(
-            height: 20,
-          ),
-          BestsellerListViewItem(),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
