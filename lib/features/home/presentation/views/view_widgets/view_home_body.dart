8a2c5f5584e6a22e35a28ca3594dfe1a3c9687c8
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-07-31 09:12:47 +0300

    split code to be more readable

--- lib/features/home/presentation/views/view_widgets/view_home_body.dart
+++ lib/features/home/presentation/views/view_widgets/view_home_body.dart
@@ -1,7 +1,6 @@
-import 'package:bookbox/core/utils/assets.dart';
 import 'package:flutter/material.dart';
-
 import 'custom_app_bar.dart';
+import 'custom_list_view_item.dart';
 
 class ViewHomeBody extends StatelessWidget {
   const ViewHomeBody({super.key});
@@ -13,29 +12,6 @@ class ViewHomeBody extends StatelessWidget {
         CustomAppBar(),
         CustomListViewItem()
       ],
-    );
-  }
-}
-
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height*.3,
-      child: AspectRatio(
-        aspectRatio: 2.7/4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.yellow,
-            image:const DecorationImage(
-              fit: BoxFit.fill,
-              image:AssetImage( AssetsBookData.test)),
-          ),
-        ),
-      ),
     );
   }
 }
