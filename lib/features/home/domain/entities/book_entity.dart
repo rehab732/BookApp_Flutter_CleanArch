272017c0d6bbf3ff6262047c8d625d6ba713fb7f
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-08-02 06:24:42 +0300

    start in domain layer and create book entity

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,11 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final num price;
+  final num rating;
+  final String autherName;
+
+  BookEntity(this.image, this.title, this.price, this.rating, this.autherName);
+  
+
+}
