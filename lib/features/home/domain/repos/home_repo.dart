ac2af3c0ef88bf85e9f5c9d570efacdecae9f0dc
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-08-02 07:22:10 +0300

    use either class

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,7 +1,8 @@
+import 'package:bookbox/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 import '../entities/book_entity.dart';
-
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchBooks();
-  Future<List<BookEntity>> fetchNewBooks();
+  Future<Either<Failure,List<BookEntity>>>fetchBooks();
+  Future<Either<Failure,List<BookEntity>>> fetchNewBooks();
 
 }
