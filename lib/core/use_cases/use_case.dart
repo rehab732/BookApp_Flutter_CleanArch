fafadfc8ca7136bca25ff7fc0c39e14da703759e
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-08-02 07:53:04 +0300

    refactor use case to be generic

---
+++ lib/core/use_cases/use_case.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+
+import '../errors/failure.dart';
+
+abstract class UseCase<T, P> {
+  Future<Either<Failure, T>> call([P p]);
+}
+class NoParam {}
--- lib/features/home/domain/use_cases/fetch_featuerd_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featuerd_books_use_case.dart
@@ -1,18 +1,18 @@
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failure.dart';
+import '../../../../core/use_cases/use_case.dart';
 import '../entities/book_entity.dart';
 import '../repos/home_repo.dart';
 
-class FeachFeturedBooksUseCase {
+class FeachFeturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
 
   FeachFeturedBooksUseCase(this.homeRepo);
-  Future<Either<Failure,List<BookEntity>>>fetchBooks(){
-
-    //additional code 
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? p]) async {
     return homeRepo.fetchBooks();
   }
-
-  
 }
+
+
