b1aef33f9250ef83ce7d799ba88e84584c2be29c
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-08-02 09:05:19 +0300

    fetch remote data from api

---
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,39 @@
+import 'package:bookbox/core/utils/api_service.dart';
+import 'package:bookbox/features/home/data/models/book_model/book_model.dart';
+import 'package:bookbox/features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchBooks();
+  Future<List<BookEntity>> fetchNewBooks();
+}
+
+class HomeRemoteDataSourceImplementation extends HomeRemoteDataSource {
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImplementation(this.apiService);
+  @override
+  Future<List<BookEntity>> fetchBooks() async {
+    var data = await apiService.get(
+        endpoint: 'volumes?Filtering=free-ebooks&Sorting=newest&q=programming');
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+@override
+  Future<List<BookEntity>> fetchNewBooks() async {
+    var data = await apiService.get(
+        endpoint: 'volumes?Filtering=free-ebooks&q=programming');
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+      List<BookEntity> books = [];
+    for (var bookMap in data['items']) {
+      books.add(BookModel.fromJson(bookMap));
+    }
+    return books;
+  }
+
+  
+}
