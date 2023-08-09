b100592faaf53de78cc67a951ca534b87f789e05
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-08-10 02:44:35 +0300

    finish news books cubit

--- lib/features/home/presentation/manager/newest_books_cubit/new_books_cubit.dart
+++ lib/features/home/presentation/manager/newest_books_cubit/new_books_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookbox/features/home/domain/entities/book_entity.dart';
 import 'package:meta/meta.dart';
+
+import '../../../domain/use_cases/fetch_news_books_use_case.dart';
 
 part 'new_books_state.dart';
 
 class NewBooksCubit extends Cubit<NewBooksState> {
-  NewBooksCubit() : super(NewBooksInitial());
+  NewBooksCubit(this.fetchNewsBooksUseCase) : super(NewBooksInitial());
+  final FetchNewsBooksUseCase fetchNewsBooksUseCase;
+  Future<void> fetchFeaturedBooks() async {
+    emit(NewBooksLoading());
+    var result = await fetchNewsBooksUseCase.call();
+    result.fold((fauiler) {
+      emit(NewBooksFauiler(fauiler.message));
+    }, (books) {
+      emit(NewBooksSuccess(books));
+    });
+  }
+
 }
--- lib/features/home/presentation/manager/newest_books_cubit/new_books_state.dart
+++ lib/features/home/presentation/manager/newest_books_cubit/new_books_state.dart
@@ -4,3 +4,15 @@ part of 'new_books_cubit.dart';
 sealed class NewBooksState {}
 
 final class NewBooksInitial extends NewBooksState {}
+
+final class NewBooksLoading extends NewBooksState {}
+
+final class NewBooksSuccess extends NewBooksState {
+  final List<BookEntity> books;
+   NewBooksSuccess( this.books);
+}
+
+final class NewBooksFauiler extends NewBooksState {
+  final String message;
+  NewBooksFauiler(this.message);
+}
