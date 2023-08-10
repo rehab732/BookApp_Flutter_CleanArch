1f0e27d322c1646785f45e4c28e2a26b5fefdfc1
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-08-10 04:20:32 +0300

    fix error

--- lib/features/home/presentation/views/view_widgets/books_list_view.dart
+++ lib/features/home/presentation/views/view_widgets/books_list_view.dart
@@ -11,6 +11,7 @@ class BooksListView extends StatelessWidget {
     return SizedBox(
       height: MediaQuery.of(context).size.height * .3,
       child: ListView.builder(
+        itemCount: books.length,
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) {
             return  Padding(
